bddf5dbf58383a6d14f86c1c4a1dea88b5a46929
Oegrem <laemmle.tobias@web.de>
2014-11-06 16:59:56 +0100

    without jsp

---
+++ src/LoginServlet.java
@@ -0,0 +1,57 @@
+
+
+import java.awt.List;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.connector.Request;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private ArrayList<String> usernameList = new ArrayList<String>();
+	
+    /**
+     * Default constructor. 
+     */
+    public LoginServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		usernameList.add(request.getParameter("username"));
+		PrintWriter pWriter = response.getWriter();
+		RequestDispatcher dispatcher1 = request.getRequestDispatcher("/index1.html");
+		dispatcher1.include(request, response);
+		for(String username : usernameList){
+		pWriter.print("<tr><td>"+username+"</td><td>0</td></tr>");
+		}
+		RequestDispatcher dispatcher2 = request.getRequestDispatcher("/index2.html");
+		dispatcher2.include(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
